cf79bb6c51344f9832624898ef553b03b301eecf
A19211565 <A19211565@FCA-CC-C22.inet.uady.mx>
2022-10-05 19:10:20 -0500

    Aritmetica

---
+++ src/main/java/com/fca/calidad/CalidadSoftware_Celeste_Concha/Aritmetica.java
@@ -0,0 +1,14 @@
+package com.fca.calidad.CalidadSoftware_Celeste_Concha;
+
+public class Aritmetica {
+	private float ultimoResultado;
+	public float suma(float primerSumando, float segundoSumando) {
+		return ultimoResultado = primerSumando + segundoSumando;
+	}
+	public float resta(float minuendo, float sustraendo) {
+		return ultimoResultado = minuendo - sustraendo;
+	}
+	
+	
+
+}
