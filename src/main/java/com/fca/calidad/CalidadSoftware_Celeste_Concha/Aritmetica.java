3704217a3dffb69df8c1fe0685f93a7dcda26ddf
celeste <celeste@DESKTOP-31C2EBR>
2022-10-16 01:02:41 -0500

    Aritmetica

--- src/main/java/com/fca/calidad/CalidadSoftware_Celeste_Concha/Aritmetica.java
+++ src/main/java/com/fca/calidad/CalidadSoftware_Celeste_Concha/Aritmetica.java
@@ -8,7 +8,23 @@ public class Aritmetica {
 	public float resta(float minuendo, float sustraendo) {
 		return ultimoResultado = minuendo - sustraendo;
 	}
+
+public float multiplicacion(float primerFactor, float segundoFactor) {
+	return ultimoResultado = primerFactor * segundoFactor;
+}
+public float division(float dividendo, float divisor) {
+	return ultimoResultado = dividendo / divisor;
+}
 	
+public int divisionEntera(int dividendo, int divisor)
+{
+return dividendo/divisor;
+}
+
+public float getUltimoResultado() {
+	return ultimoResultado;
+}
+}
 	
 
-}
+
