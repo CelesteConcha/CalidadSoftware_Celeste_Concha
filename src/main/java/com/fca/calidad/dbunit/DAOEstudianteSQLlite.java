d1717145d28f3ce2a0abafb38c6fec0bb7fae187
celeste <celeste@DESKTOP-31C2EBR>
2022-11-21 23:24:19 -0600

    Dbunit se presentó un problema de conexion

--- src/main/java/com/fca/calidad/dbunit/DAOEstudianteSQLlite.java
+++ src/main/java/com/fca/calidad/dbunit/DAOEstudianteSQLlite.java
@@ -7,10 +7,17 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class DAOEstudianteSQLlite implements IDAOEstudiante {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+public class DAOEstudianteSQLlite implements IDAOEstudiante{
+	
 	private static final String DRIVER_NAME = "org.sqlite.JDBC";
-	private static final String DB_URL = "jdbc:sqlite:C:\\Users\\A19211565\\Downloads\\Git\\CalidadSoftware_Celeste_Concha\\src\\Alumnos.db";
+	private static final String DB_URL = "jdbc:sqlite:.\\src\\resources\\Alumnos.db";
 	private static final String ID= "";
 	private static final String PASS= "";
 	
@@ -102,7 +109,7 @@ public class DAOEstudianteSQLlite implements IDAOEstudiante {
 				result = true;
 			}
 			System.out.println("\n");
-			System.out.println("Estudiante eliminado con exito");
+			System.out.println("¡Estudiante eliminado con exito!:D");
 			System.out.println(">> Return: " + result + "\n");
 			// Close connection with the database
 			conn.close();
@@ -115,7 +122,8 @@ public class DAOEstudianteSQLlite implements IDAOEstudiante {
 		return result;
 		
 	}
-
+	
+	
 	@Override
 	public boolean updateEmailEstudiante(Estudiante e) {
 		
@@ -138,7 +146,46 @@ public class DAOEstudianteSQLlite implements IDAOEstudiante {
 				result = true;
 			}
 			System.out.println("\n");
-			System.out.println("Correo de alumno con ID: " + aux + " actualizado");
+			System.out.println("Email de alumno con ID: " + aux + " actualizado!");
+			System.out.println(">> Return: " + result + "\n");
+			// Close connection with the database
+			conn.close();
+			preparedStatement.close();
+
+		} catch (Exception e2) {
+			System.out.println(e2);
+		}
+		// Return statement
+		return result;
+		
+		
+	}
+	
+	
+	//Agregamos un metodo más de manera que, ahora también podamos realizar la actualización del nombre.
+	@Override
+	public boolean updateNombreEstudiante(Estudiante e) {
+		
+		Connection conn = getConnection();
+		boolean result = false;
+
+		try {
+			// Declare statement query to run
+			PreparedStatement preparedStatement;
+			preparedStatement = conn.prepareStatement("UPDATE Estudiante SET nombre = ? WHERE id = ?");
+			// Set the values to match in the ? on query
+			preparedStatement.setString(1, e.getNombre());
+			
+			String aux = Integer.toString(e.getId());
+			
+			preparedStatement.setString(2, aux);
+
+			// Return the result of connection and statement
+			if (preparedStatement.executeUpdate() >= 1) {
+				result = true;
+			}
+			System.out.println("\n");
+			System.out.println("Nombre de alumno con ID: " + aux + " actualizado");
 			System.out.println(">> Return: " + result + "\n");
 			// Close connection with the database
 			conn.close();
@@ -200,7 +247,6 @@ public class DAOEstudianteSQLlite implements IDAOEstudiante {
 		return retrieved;	
 	
 	}
-	
-
 
 }
+
